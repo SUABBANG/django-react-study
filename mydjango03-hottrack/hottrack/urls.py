b3c315a74518eefc3924b3f7b7e9b7d3b64dad93
ssuangg <115128140+ssuangg@users.noreply.github.com>
2024-07-24 17:38:55 +0900

    section04

---
+++ mydjango03-hottrack/hottrack/urls.py
@@ -0,0 +1,8 @@
+# hottrack/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(route="", view=views.index),
+]
---
+++ mydjango03-hottrack/hottrack/views.py
@@ -0,0 +1,26 @@
+import json
+from urllib.request import urlopen
+
+from django.db.models import QuerySet, Q
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from hottrack.models import Song
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    query = request.GET.get("query", "").strip()
+    song_qs: QuerySet = Song.objects.all()
+
+    if query:
+        song_qs = song_qs.filter(
+            Q(name__icontains=query)
+            | Q(artist_name__icontains=query)
+            | Q(album_name__icontains=query)
+        )
+        print(song_qs)
+    return render(
+        request=request,
+        template_name="hottrack/index.html",
+        context={"song_list": song_qs, "query": query},
+    )
