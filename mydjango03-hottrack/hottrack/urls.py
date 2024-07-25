02e70391c8b8b825211640b3ca0f5d140a4f5fff
ssuangg <115128140+ssuangg@users.noreply.github.com>
2024-07-25 14:06:17 +0900

    django03 end

--- mydjango03-hottrack/hottrack/urls.py
+++ mydjango03-hottrack/hottrack/urls.py
@@ -2,7 +2,13 @@
 
 from django.urls import path
 from . import views
+from . import converters  # noqa
+
 
 urlpatterns = [
     path(route="", view=views.index),
+    path(route="archives/<date:release_date>", view=views.index),
+    path(route="<int:pk>/cover.png", view=views.cover_png),
+    path(route="export.csv", view=views.export_csv),
+    path(route=r"^export\.(?P<format>(csv|xlsx))$", view=views.export, name="export"),
 ]
