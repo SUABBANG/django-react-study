9e3703909d140017933a29d2461540c96ebfe504
ssuangg <115128140+ssuangg@users.noreply.github.com>
2024-08-06 18:35:48 +0900

    init setting

---
+++ myproj/mysite/urls.py
@@ -0,0 +1,13 @@
+from django.apps import apps
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("",include("core.urls")),
+    path("accounts/",include("accounts.urls")),
+    path("admin/", admin.site.urls),
+]
+if apps.is_installed("debug_toolbar"):
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
